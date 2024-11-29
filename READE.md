2c1a6a9eef96cdd558582fd862a0b423fedc4975
Daiani <daianirodrigues991@gmail.com>
2024-11-29 13:05:25 -0300

    finalizando projeto

---
+++ READE.md
@@ -0,0 +1,18 @@
+# Lacrei Saúde
+
+### Descrição
+
+Esse projeto foi feito como o teste técnico para a vaga de desenvolvedora Front end para a ONG Lacrei Saúde.
+
+### Tecnologias utilizadas
+
+- HTML
+- CSS
+- JavaScript
+
+### Principais recursos
+
+- HTML Semántico: Utilizando as tags adequadas para construir o corpo das páginas.
+- Variáveis CSS: Com a utilização de variáveis CSS, o desenvolvimento foi mais fácil, principalmente por seguir o design system fornecido.
+- Fiel ao design system: Foi seguido fielmente as cores presentes no Figma.
+
--- script.js
+++ script.js
@@ -1,25 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
-    emailjs.init("YOUR_USER_ID"); // Substitua YOUR_USER_ID pelo seu ID de usuário do EmailJS
-
     document.getElementById('contact-form').addEventListener('submit', function(event) {
         event.preventDefault(); 
 
-       
-        const templateParams = {
-            name: document.getElementById('name').value,
-            email: document.getElementById('email').value,
-            subject: document.getElementById('subject').value,
-            message: document.getElementById('message').value
-        };
+        const nome = document.getElementById('name').value
+    
+        const message = `Obrigado pelo contato ${nome}!`
 
-        emailjs.send('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', templateParams)
-        .then(function(response) {
-            console.log('SUCCESS!', response.status, response.text);
-            alert('Mensagem enviada com sucesso!'); 
-
-        }, function(error) {
-            console.log('FAILED...', error);
-            alert('Ocorreu um erro ao enviar a mensagem. Tente novamente.');
-        });
+        alert(message)
     });
 });
